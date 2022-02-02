86c07e4e479c3036eb11f4e3d55a5f79cbc17dd5
jamesserra <jamesserra4@gmail.com>
2022-02-02 14:41:51 -0500

    table updates

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,56 +1,81 @@
+import React from 'react';
+import TableInput from './TableInput'
 
-import React, { useEffect, useState } from 'react';
-import TableInput from './TableInput';
+const curr_API = 'https://altexchangerateapi.herokuapp.com/latest';
 
-const curr_API = 'https://altexchangerateapi.herokuapp.com/latest'
+class Table extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      base: 'USD',
+      rates: '',
+    }
+  }
+  componentDidMount() {
+    this.getRates(this.state.base);
+  }
+  setBase = (e) => {
+    this.setState({ base: e.value });
+    this.getRates(e.target.value);
+  }
 
-const Table = () => {
-  const [base, setBase] = useState([])
-  const [fromCurrency, setFromCurrency] = useState()
-  const [exchangeRate, setRates] = useState([])
-
-  useEffect(() => {
-    fetch(curr_API)
+  getRates = (base) => {
+    fetch(`${curr_API}?from=${base}`)
       .then(res => res.json())
       .then(data => {
-        setBase([data.base, ...Object.keys(data.rates)])
-        setRates(data.rates)
-        setFromCurrency(data.base)
+        const rates = Object.keys(data.rates)
+        console.log(data)
+        const setRate = (data.rates)
+        console.log(setRate)
+        this.setState({ rates, base, setRate });
       })
+  }
 
-  }, [])
-
-  return (
-    <>
-      <div className="container">
-        <div className="row card1">
-          <div className="col-12">
-            <h1 className="title">Currency Table</h1>
-            <TableInput
-              currencyOptions={base}
-              onChangeCurrency={e => setFromCurrency(e.target.value)}
-            />
-            <table className="table table-striped table-hover">
-              <thead>
-                <tr className='text-center'>
-                  <th>Exchange Rate</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  Object.keys(exchangeRate).map((code) => (
-                    <tr key={code} className='text-center'>
-                      <td>{code} {exchangeRate[code] * 1}</td>
+  render() {
+    const { base, rates, setRate } = this.state;
+    return (
+      <>
+        <div className="container">
+          <div className="row card1">
+            <div className="col-12">
+              <h1 className="title">Currency Table</h1>
+              <div className='element'>
+                <form className='d-inline-flex justify-content-center'>
+                  <h4 className='mt-3'>1</h4>
+                  <select value={base} onChange={this.setBase} className="form-control text-center w-75 mb-2">
+                    {Object.keys(TableInput).map(option => <option key={option} value={option}>{option}</option>)}
+                  </select>
+                </form>
+                <table className="table table-striped table-hover">
+                  <thead>
+                    <tr className='text-center'>
+                      <th>Exchange Rate</th>
                     </tr>
-                  ))
-                }
-              </tbody>
-            </table>
+                  </thead>
+                  <tbody>
+                    {
+                      Object.keys(rates).map((code) => (
+                        <tr key={code} value={setRate} className='text-center'>
+                          <td>{rates[code]}</td>
+                          {
+                            Object.keys(setRate).map((code) => (
+                              <tr key={code} className='text-center'>
+                                <td>{code} {setRate[code] * 1}</td>
+                              </tr>
+                            ))
+                          }
+                          {/* <td>{base}</td> */}
+                        </tr>
+                      ))
+                    }
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    )
+  }
 }
 export default Table;
-
--- src/pages/TableInput.js
+++ src/pages/TableInput.js
@@ -1,19 +1,102 @@
-import React from 'react';
-
-export default function TableInput(props) {
-  const {
-    currencyOptions,
-    selectedCurrency,
-    onChangeCurrency,
-  } = props
-
-  return (
-    <div className='element'>
-      <select value={selectedCurrency} onChange={onChangeCurrency}>
-        {currencyOptions.map(option => (
-          <option key={option} value={option}>{option}</option>
-        ))}
-      </select>
-    </div>
-  )
+const TableInput = {
+  AUD: {
+    symbol: '$',
+  },
+  BGN: {
+    symbol: 'лв',
+  },
+  BRL: {
+    symbol: 'R$',
+  },
+  CAD: {
+    symbol: '$',
+  },
+  CHF: {
+    symbol: 'Fr',
+  },
+  CNY: {
+    symbol: '¥',
+  },
+  CZK: {
+    symbol: 'Kč',
+  },
+  DKK: {
+    symbol: 'kr',
+  },
+  EUR: {
+    symbol: '€',
+  },
+  GBP: {
+    symbol: '£',
+  },
+  HKD: {
+    symbol: '$',
+  },
+  HRK: {
+    symbol: 'kn',
+  },
+  HUF: {
+    symbol: 'Ft',
+  },
+  IDR: {
+    symbol: 'Rp',
+  },
+  ILS: {
+    symbol: '₪',
+  },
+  INR: {
+    symbol: '₹',
+  },
+  ISK: {
+    symbol: 'kr',
+  },
+  JPY: {
+    symbol: '¥',
+  },
+  KRW: {
+    symbol: '₩',
+  },
+  MXN: {
+    symbol: '$',
+  },
+  MYR: {
+    symbol: 'RM',
+  },
+  NOK: {
+    symbol: 'kr',
+  },
+  NZD: {
+    symbol: '$',
+  },
+  PHP: {
+    symbol: '₱',
+  },
+  PLN: {
+    symbol: 'zł',
+  },
+  RON: {
+    symbol: 'lei',
+  },
+  RUB: {
+    symbol: '₽',
+  },
+  SEK: {
+    symbol: 'kr',
+  },
+  SGD: {
+    symbol: '$',
+  },
+  THB: {
+    symbol: '฿',
+  },
+  TRY: {
+    symbol: '₺',
+  },
+  USD: {
+    symbol: '$',
+  },
+  ZAR: {
+    symbol: 'R',
+  },
 }
+export default TableInput;
